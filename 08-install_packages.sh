67f9c9ebfc857d037e98061d4697d70626bb5295
Ravikiran1313 <kravi9010@gmail.com>
2023-12-17 21:51:49 +0530

    second change

---
+++ 08-install_packages.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if ( $1 -ne 0 )
+    then 
+        echo -e "$R error::while $2"
+    else 
+        echo -e "$G sucessfully $2"    
+}
+
+if ( $ID -ne 0 )
+    then 
+        echo -e "$R error:: your are not a root user"
+    else 
+        echo -e "$G your a root user" 
+
+for package in $@
+do
+    yum list installed $package &>> $LOGFILE
+    if( $package -ne 0 )
+    then   
+        yum install $package -y &>> LOGFILE
+        VALIDATE $? install_package
+    else
+        echo -e "$Y package already installed"
