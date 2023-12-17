1032e7b3e6e036fd77233023db2e650c0c1063db
Ravikiran1313 <kravi9010@gmail.com>
2023-12-17 19:10:54 +0530

    second change

--- 06-functions_log.sh
+++ 06-functions_log.sh
@@ -7,4 +7,12 @@ then
     echo "you are not a root user"
 else
     echo "you are a root user"    
-fi    
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Error while installing"
+else
+    echo "installed successfully"
