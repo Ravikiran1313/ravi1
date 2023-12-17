10cfddfca3d9bc59e65ac69d70c3efdb2691bd77
Ravikiran1313 <kravi9010@gmail.com>
2023-12-17 19:30:27 +0530

    second change

---
+++ 07-logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F -%H -%M -%S)
+LOGFILE=/tmp/"$0_$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+then
+    echo "Error::$2"
+    exit 1
+else
+    echo "$2 successfully"
+fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "you are not a root user"
+    exit 1
+else
+    echo "you are a root user"    
+fi
+
+yum install mysql -y &>> LOGFILE
+
+VALIDATE $? SQL_installation
+
+yum install git -y
+
+VALIDATE $? git_installation
