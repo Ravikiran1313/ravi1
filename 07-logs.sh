50ae8a0071f5ca1e8e4940b3ed701ffc7365bc80
Ravikiran1313 <kravi9010@gmail.com>
2023-12-17 19:51:57 +0530

    second change

--- 07-logs.sh
+++ 07-logs.sh
@@ -12,19 +12,19 @@ LOGFILE="/tmp/$0_$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
 then
-    echo "$R Error::$N $2"
+    echo -e "$R Error::$N $2"
     exit 1
 else
-    echo "$G $2 $N successfully"
+    echo -e "$G $2 $N successfully"
 fi
 }
 
 if [ $ID -ne 0 ]
 then
-    echo "you are not a root user"
+    echo -e "you are not a root $R user"
     exit 1
 else
-    echo "you are a root user"    
+    echo -e "you are a root $G user"    
 fi
 
 yum install mysql -y &>> $LOGFILE
